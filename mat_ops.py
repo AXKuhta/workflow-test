bbe407ab180389b79d3145734cae1b5a514930f4
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2022-09-29 22:43:07 +0800

    Matrix ops: mul row col rowswap rowk rowkadd

--- mat_ops.py
+++ mat_ops.py
@@ -67,4 +67,74 @@ def transpose(a):
 
 	return b
 	
+def mul(a, b):
+	t = transpose(b)
+	mat = []
 
+	for i in range( len(a) ):
+		row = []
+
+		for j in range( len(t) ):
+			row.append( vec.dot(a[i], t[j]) )
+
+		mat.append(row)
+
+	return mat
+
+def row(a, i):
+	if i > len(a) - 1:
+		raise Exception("Index out of range")
+
+	return a[i]
+
+def col(a, i):
+	t = transpose(a)
+
+	if i > len(t) - 1:
+		raise Exception("Index out of range")
+
+	return t[i]
+
+def rowswap(a, i, j, do_copy=True):
+	if do_copy:
+		a = [] + a
+
+	if i > len(a) - 1:
+		raise Exception("Index out of range")
+
+	if j > len(a) - 1:
+		raise Exception("Index out of range")
+
+	t = a[i]
+	a[i] = a[j]
+	a[j] = t
+
+	return a
+
+# a[i] = k*a[i]
+def rowk(a, i, k, do_copy=True):
+	if do_copy:
+		a = [] + a
+
+	if i > len(a) - 1:
+		raise Exception("Index out of range")
+
+	a[i] = vec.mul(a[i], k, do_copy)
+
+	return a
+
+# a[i] = a[i] + k*a[j]
+def rowkadd(a, i, j, k, do_copy=True):
+	if do_copy:
+		a = [] + a
+
+	if i > len(a) - 1:
+		raise Exception("Index out of range")
+
+	if j > len(a) - 1:
+		raise Exception("Index out of range")
+
+	krow = vec.mul(a[j], k, do_copy)
+	a[i] = vec.add(a[i], krow, do_copy)
+
+	return a
--- test_mat.py
+++ test_mat.py
@@ -1,4 +1,5 @@
 import mat_ops as mat
+import vec_ops as vec
 
 def test_equal():
 	a = [[1,2], [3,4]]
@@ -37,3 +38,38 @@ def test_transpose():
 	assert mat.equal( mat.transpose(a2), t2 )
 	assert mat.equal( mat.transpose(a3), t3 )
 
+def test_mul():
+	a = [[1,0], [0,1]]
+	b = [[3], [7]]
+
+	assert mat.equal( mat.mul(a, b), [[3],[7]] )
+
+def test_row():
+	a = [[1,2], [3,4]]
+
+	assert vec.equal( mat.row(a, 0), [1,2] )
+
+def test_col():
+	a = [[1,2], [3,4]]
+
+	assert vec.equal( mat.col(a, 0), [1,3] )
+
+def test_rowswap():
+	a = [[1,2], [3,4]]
+	b = [[3,4], [1,2]]
+
+	assert mat.equal( mat.rowswap(a, 0, 1), b )
+
+def test_rowk():
+	a = [[1,0], [0,1]]
+	k = 5
+	c = [[5,0], [0,1]]
+
+	assert mat.equal( mat.rowk(a, 0, k), c )
+
+def test_rowkadd():
+	a = [[1,0], [0,1]]
+	k = -5
+	c = [[1,-5], [0,1]]
+
+	assert mat.equal( mat.rowkadd(a, 0, 1, k), c )
