c118bcacad9ffa4763550c436e087c15e5e09e28
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2022-12-21 15:39:30 +0800

    Add tests for series, clarify limitations in arcsin(x) and arccos(x)

--- series/ops.py
+++ series/ops.py
@@ -1,4 +1,5 @@
 import vec.ops as vec
+import math
 
 def fact_arr(n):
 	"""
@@ -21,30 +22,56 @@ def pow_arr(x, n):
 	
 	return [x**i for i in range(1, n+1)]
 
-exp_derivatives_at_zero = [1]
-sin_derivatives_at_zero = [0, 1, 0, -1]
-arcsin_derivatives_at_zero = [1, 0]
-cos_derivatives_at_zero = [0, -1, 0, 1]
-arccos_derivatives_at_zero = [-1, 0]
-
-exp_at_zero = 1
-sin_at_zero = 0
-cos_at_zero = 1
-arcsin_at_zero = 0
-arccos_at_zero = 3.14/2
-
-def init_approx_exp(n):
-	"""
-	Получить функцию для вычисления exp(x) при помощи ряда Маклорена
-	"""
-
-	return init_approx_series(n, exp_at_zero, exp_derivatives_at_zero)
-	
-
 def init_approx_series(n, fx_at_zero, dfx_at_zero):
+	"""
+	Получить функцию для примерного вычисления при помощи ряда Маклорена
+
+	``n`` - количество членов в ряде
+	``fx_at_zero`` - значение функции в точке ноль
+	``dfx_at_zero`` - значение производных функции в точке ноль
+	"""
+
 	dfx_series = (dfx_at_zero*(1 + n // len(dfx_at_zero)))[:n]
 
 	k = [a*b for a, b in zip( vec.invert(fact_arr(n)) , dfx_series)]
 
 	return lambda x: fx_at_zero + vec.dot(k, pow_arr(x, n))
 
+def init_approx_exp(n):
+	"""
+	Получить функцию для вычисления exp(x) при помощи ряда Маклорена
+	"""
+
+	return init_approx_series(n, 1, [1])
+
+def init_approx_sin(n):
+	"""
+	Получить функцию для вычисления sin(x) при помощи ряда Маклорена
+	"""
+
+	return init_approx_series(n, 0, [1, 0, -1, 0])
+
+def init_approx_cos(n):
+	"""
+	Получить функцию для вычисления cos(x) при помощи ряда Маклорена
+	"""
+
+	return init_approx_series(n, 1, [0, -1, 0, 1])
+
+def init_approx_arcsin(n):
+	"""
+	Получить функцию для вычисления arcsin(x) при помощи ряда Маклорена
+
+	Предупреждение: производная arcsin(x) в точке 0 не является периодичной; не используйте n > 10 или x > 0.5
+	"""
+
+	return init_approx_series(n, 0, [1, 0, 1, 0, 9, 0, 225, 0, 11025, 0])
+
+def init_approx_arccos(n):
+	"""
+	Получить функцию для вычисления arccos(x) при помощи ряда Маклорена
+
+	Предупреждение: производная arccos(x) в точке 0 не является периодичной; не используйте n > 10 или x > 0.5
+	"""
+
+	return init_approx_series(n, math.pi/2, [-1, 0, -1, 0, -9, 0, -225, 0, -11025, 0])
--- series/test_series.py
+++ series/test_series.py
@@ -1,8 +1,34 @@
 import series.ops as series
 import vec.ops as vec
+import math
 
 def test_approx_exp():
 	exp = series.init_approx_exp(20)
 	
 	assert vec.value_close(exp(1), 2.71828182)
 
+def test_approx_sin():
+	sin = series.init_approx_sin(20)
+
+	assert vec.value_close(sin(0.0), 0.0)
+	assert vec.value_close(sin(math.pi/2.0), 1.0)
+	assert vec.value_close(sin(1.0), math.sin(1.0))
+
+def test_approx_cos():
+	cos = series.init_approx_cos(20)
+
+	assert vec.value_close(cos(0.0), 1.0)
+	assert vec.value_close(cos(math.pi/2.0), 0.0)
+	assert vec.value_close(cos(1.0), math.cos(1.0))
+
+def test_approx_arcsin():
+	arcsin = series.init_approx_arcsin(10)
+
+	assert vec.value_close(arcsin(0.0), 0.0)
+	assert vec.value_close(arcsin(0.5), math.asin(0.5))
+
+def test_approx_arccos():
+	arccos = series.init_approx_arccos(10)
+
+	assert vec.value_close(arccos(0), math.pi/2)
+	assert vec.value_close(arccos(0.5), math.acos(0.5))
